54cb78d488c0e542517cb6996053662afa30c0a2
Matthis Perrin <matthis.perrin@gmail.com>
2019-11-01 22:33:24 +0100

    Add prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import styles from "./App.module.sass";
-import castor from "../images/castor.png";
+import React from 'react';
+import styles from './App.module.sass';
+import castor from '../images/castor.png';
 
 function App() {
   return (
