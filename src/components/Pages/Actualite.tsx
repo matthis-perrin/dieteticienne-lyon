db685530da5c59a4fdfc634285988b78776df931
Matthis Perrin <matthis.perrin@gmail.com>
2019-12-15 18:21:02 +0100

    Add function to truncate text + an example

--- src/components/Pages/Actualite.tsx
+++ src/components/Pages/Actualite.tsx
@@ -15,6 +15,54 @@ interface ActualiteData {
   pdf: string;
 }
 
+function truncateText(
+  text: string,
+  options: {
+    fontSize: number;
+    fontFamily: string;
+    lineHeight: number;
+    width: number;
+    height: number;
+  }
+): string {
+  const {fontSize, fontFamily, width, height, lineHeight} = options;
+  const randomId = Math.round(Math.random() * 1e10);
+  const boxId = `measure-text-${randomId}`;
+  const box = document.createElement('div');
+  box.setAttribute('id', boxId);
+  box.style.position = 'absolute';
+  box.style.left = '-99999px';
+  box.style.top = '-99999px';
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+  box.style.fontSize = `${fontSize}px`;
+  box.style.fontFamily = fontFamily;
+  box.style.lineHeight = `${lineHeight}px`;
+
+  document.body.appendChild(box);
+
+  const words = text.split(' ');
+  for (let numberOfWord = 1; numberOfWord < words.length; numberOfWord++) {
+    const content = words.slice(0, numberOfWord);
+    box.innerHTML = content.join(' ') + '...';
+    if (box.scrollHeight > box.offsetHeight) {
+      return words.slice(0, numberOfWord - 1).join(' ') + '...';
+    }
+  }
+  return text;
+}
+
+const test =
+  'C’est un bouton de fleur, déjà connu en Mésopotamie 1700 ans avant J-C, et plus tard les croisés l’ont utilisé pour combattre... les rages de dent.';
+const truncated = truncateText(test + test + test + test + test + test + test + test, {
+  fontSize: 16,
+  fontFamily: 'Roboto',
+  lineHeight: 20,
+  width: 300,
+  height: 60,
+});
+console.log(truncated);
+
 const actualites: ActualiteData[] = [
   {
     title: 'La canelle',
